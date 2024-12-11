e64fb30c7ea01b1e24d307c0cf8f78a691e2a626
minji2219 <200146@jnu.ac.kr>
2024-12-11 21:47:18 +0900

    feat: 휴일 정보 입력

---
+++ src/HolidayInfo.js
@@ -0,0 +1,10 @@
+export const HolidayInfo = [
+  {month: 1, date: 1},
+  {month: 3, date: 1},
+  {month: 5, date: 5},
+  {month: 6, date: 6},
+  {month: 8, date: 15},
+  {month: 10, date: 3},
+  {month: 10, date: 9},
+  {month: 12, date: 25},
+];
