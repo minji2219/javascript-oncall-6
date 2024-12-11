0edb65fe0416fb9dcdf88596383671546e341dd9
minji2219 <200146@jnu.ac.kr>
2024-12-12 02:07:15 +0900

    feat: 전체 로직 실행

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import {Console} from '@woowacourse/mission-utils';
+import InputView from './InputView.js';
+import {Assigning} from './Assigning.js';
+
 class App {
-  async run() {}
+  async run() {
+    const date = await InputView.readDate();
+    await InputView.readWeekDayWork();
+    new Assigning(date, InputView.worker);
+  }
 }
 
 export default App;
